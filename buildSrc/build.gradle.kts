92b7ec4407ad97c647ce2c90242fd0fbef422bfa
Bret Calvey <bacalv@gmail.com>
2025-02-11 17:17:21 +0000

    PDF4K-000 [BAC] Added docker plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,5 +8,6 @@ repositories {
 }
 
 dependencies {
+    implementation("com.bmuschko:gradle-docker-plugin:9.4.0")
     implementation(libs.kotlin.gradle.plugin)
 }
--- buildSrc/src/main/kotlin/buildlogic.kotlin-application-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.kotlin-application-conventions.gradle.kts
@@ -1,4 +1,19 @@
 plugins {
     id("buildlogic.kotlin-common-conventions")
+    id("com.bmuschko.docker-remote-api")
+    id("com.bmuschko.docker-java-application")
     application
+}
+
+repositories {
+    gradlePluginPortal()
+}
+
+docker {
+    javaApplication {
+        baseImage.set("openjdk:21-ea-1-slim")
+        maintainer.set("Bret Calvey 'bacalv@gmail.com'")
+        ports.set(listOf(8080))
+        jvmArgs.set(listOf("-Xms256m", "-Xmx2048m"))
+    }
 }
