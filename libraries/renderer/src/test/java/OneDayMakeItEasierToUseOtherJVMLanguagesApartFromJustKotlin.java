e67fdccc7dd816da3edf331077c06539b33c8d3f
Bret Calvey <bacalv@gmail.com>
2025-05-07 01:54:21 +0100

    PDF4K-000 [BAC] Read the filename in this commit, that ought to do it

---
+++ libraries/renderer/src/test/java/OneDayMakeItEasierToUseOtherJVMLanguagesApartFromJustKotlin.java
@@ -0,0 +1,24 @@
+import io.pdf4k.domain.Pdf;
+import io.pdf4k.domain.Stationary;
+import io.pdf4k.testing.InMemoryRenderer;
+import kotlin.Unit;
+
+import java.io.FileOutputStream;
+
+import static io.pdf4k.dsl.PdfBuilder.Companion;
+
+public class OneDayMakeItEasierToUseOtherJVMLanguagesApartFromJustKotlin {
+    public static void main(String[] args) throws Exception {
+        Pdf pdf = Companion.pdf(null, builder -> {
+            builder.page(null, Stationary.Companion.getBlankA4Portrait(), new Stationary[0], page -> {
+                page.content(null, content -> {
+                    content.paragraph("Hello world!");
+                    return Unit.INSTANCE;
+                });
+                return Unit.INSTANCE;
+            });
+            return Unit.INSTANCE;
+        });
+        InMemoryRenderer.INSTANCE.render(pdf, new FileOutputStream("./kaboom.pdf"));
+    }
+}
